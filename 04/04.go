7640f846cd1535782a928c45c34c218776af713b
gswlee <995030368@qq.com>
2022-05-04 15:54:18 +0800

    04.二维数组中的查找

--- 04/04.go
+++ 04/04.go
@@ -1,9 +1,29 @@
 package main
 
-func Find_number(arr [][]int,target int){
-	
-}
-
-func main() {
-
+//
+// Find_number
+//  @Description: 判断数组中是否含有目标元素
+//  @param arr
+//  @param target
+//  @return bool
+//
+func Find_number(arr [][]int, target int) bool {
+	if len(arr) == 0 {
+		return false
+	}
+	if len(arr[0]) == 0 {
+		return false
+	}
+	h, w := len(arr), len(arr[0])
+	x, y := 0, w-1
+	for x < h && y >= 0 {
+		if arr[x][y] == target {
+			return true
+		} else if arr[x][y] > target {
+			y--
+		} else {
+			x++
+		}
+	}
+	return false
 }
